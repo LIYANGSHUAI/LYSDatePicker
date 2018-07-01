41fb96260d077d320974f9ad17bec714f66c51b4
李阳帅 <liyangshuai163@163.com>
2018-07-01 14:10:11 +0800

    之前的代码有点乱,现在打算重写一版

---
+++ template/LYSDatePickerDemo/LYSDatePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYSDatePickerDemo
+//
+//  Created by liyangshuai on 2018/7/1.
+//  Copyright © 2018年 liyangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ template/LYSDatePickerDemo/LYSDatePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYSDatePickerDemo
+//
+//  Created by liyangshuai on 2018/7/1.
+//  Copyright © 2018年 liyangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
